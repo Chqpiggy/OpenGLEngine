a4c165ea1e33065222cf22ccecee3d7d1d48673c
Your Name <786110020@qq.com>
2023-06-18 19:29:55 +0800

    窗口初始化

--- demo/OpenGLDemo/Main.cpp
+++ demo/OpenGLDemo/Main.cpp
@@ -1,8 +1,39 @@
-#include<iostream>
-using namespace std;
-
+#include <windows.h>
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_DESTROY:    // 窗口销毁消息
+	{
+		PostQuitMessage(0);   //  发送退出消息
+	}
+	break;
+	}
+	// 其他的消息调用缺省的消息处理程序
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
 int main()
 {
-    cout << "aaaa" << endl;
-    return 0;
+
+	WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WindowProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, L"Example", NULL };
+	::RegisterClassExW(&wc);
+	HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"OpenGLDemo", WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
+
+	::ShowWindow(hwnd, SW_SHOWDEFAULT);
+	::UpdateWindow(hwnd);
+	bool bDone = false;
+	while (true)
+	{
+		MSG msg;
+		while (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+		if (bDone)
+		{
+			break;
+		}
+	}
+	return 0;
 }
